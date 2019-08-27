b44611f7c0db6ac61c8ff994ffafaebab434cb88
natalialopatkina <natalya.lopatkina1994@gmail.com>
2019-08-27 17:19:33 +0300

    Add component assistance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Blog';
+  name:string = "Tom";
+  age:number = 24;
 }
--- src/components/Child/child.component.ts
+++ src/components/Child/child.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
 import { User } from '../../interface/user';
  
 @Component({
@@ -8,22 +9,25 @@ import { User } from '../../interface/user';
 })
 
 export class ChildComponent {
-  title = 'my-project';
-  users: User[] = [
-    {
-      name: 'Sara',
-      lastName: 'Sarasara',
-      age: 25,
-    },
+  // title = 'my-project';
+  // users: User[] = [
+  //   {
+  //     name: 'Sara',
+  //     lastName: 'Sarasara',
+  //     age: 25,
+  //   },
 
-    {
-      name: 'John',
-      age: 30,
-    },
+  //   {
+  //     name: 'John',
+  //     age: 30,
+  //   },
 
-    {
-      name: 'Mike',
-      age: 35,
-    }
-  ]
+  //   {
+  //     name: 'Mike',
+  //     age: 35,
+  //   }
+  // ]
+
+  @Input() userName: string;
+  @Input() userAge: number;
 }
